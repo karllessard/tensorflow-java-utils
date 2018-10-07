c88d036edc1984cbf4c3684f0698208c3c899cf7
Karl Lessard <karl@kubx.ca>
2018-10-07 00:36:46 -0400

    Improve unit tests

--- src/test/java/com/kubx/tensorflow/data/ValueTest.java
+++ src/test/java/com/kubx/tensorflow/data/ValueTest.java
@@ -71,7 +71,7 @@ public class ValueTest {
     int[] vector = new int[] {1, 2, 3};
     try (Tensor<Integer> t = Tensors.create(vector)) {
       IntValue value = IntValue.of(t);
-      assertBufferEquals(IntBuffer.wrap(vector), value.vector());
+      assertEquals(IntBuffer.wrap(vector), value.vector());
     }
   }
   
@@ -82,7 +82,7 @@ public class ValueTest {
     try (Tensor<Integer> t = Tensors.create(originalVector)) {
       IntValue value = IntValue.of(t);
       value.vector().put(newVector);
-      assertBufferEquals(IntBuffer.wrap(newVector), value.vector());
+      assertEquals(IntBuffer.wrap(newVector), value.vector());
     }
   }
   
@@ -116,7 +116,7 @@ public class ValueTest {
     try (Tensor<Integer> t = Tensors.create(matrix)) {
       IntValue value = IntValue.of(t).at(1);
       assertEquals(3, value.size());
-      assertBufferEquals(IntBuffer.wrap(matrix[1]), value.vector());
+      assertEquals(IntBuffer.wrap(matrix[1]), value.vector());
     }
   }
 
@@ -141,10 +141,10 @@ public class ValueTest {
       assertEquals(2, value.size());
       IntValue vector1 = value.at(0);
       assertEquals(3, vector1.size());
-      assertBufferEquals(IntBuffer.wrap(matrix[1]), vector1.vector());
+      assertEquals(IntBuffer.wrap(matrix[1]), vector1.vector());
       IntValue vector2 = value.at(1);
       assertEquals(3, vector2.size());
-      assertBufferEquals(IntBuffer.wrap(matrix[2]), vector2.vector());
+      assertEquals(IntBuffer.wrap(matrix[2]), vector2.vector());
     }
   }
   
@@ -224,7 +224,7 @@ public class ValueTest {
   }
 
   @Test
-  public void getScalarsOfLastVectorsOfSecondDimensionIn3dMatrix() {
+  public void getScalarsOfLastVectorsOfSecondDimensionInMatrix3d() {
     int[][][] matrix = new int[][][] {
       {
         {1, 2, 3}, {4, 5, 6}, {7, 8, 9}
@@ -288,146 +288,36 @@ public class ValueTest {
     }
   }
   
-/*
   @Test
-  public void getVector() {
-    int[] vector = new int[] {1, 2, 3, 4};
-    try (Tensor<Integer> t = Tensors.create(vector)) {
+  public void getVectorsAndScalarsInMatrix4d() {
+    int[][][][] matrix4d = new int[][][][]{
+      {
+        {
+          {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}
+        },
+        {
+          {13, 14, 15}, {16, 17, 18}, {19, 20, 21}, {22, 23, 24}
+        }
+      },
+      {
+        {
+          {25, 26, 27}, {28, 29, 30}, {31, 32, 33}, {34, 35, 36}
+        },
+        {
+          {37, 38, 39}, {40, 41, 42}, {43, 44, 45}, {46, 47, 48}
+        }
+      }
+    };
+    try (Tensor<Integer> t = Tensors.create(matrix4d)) {
       IntValue value = IntValue.of(t);
-      assertEquals(4L, value.size());
-      assertBufferEquals(IntBuffer.wrap(vector), value.vector());
-    }
-  }
-
-  @Test
-  public void cannotConvertScalarToVector() {
-    int scalar = 10;
-    try (Tensor<Integer> t = Tensors.create(scalar)) {
-      IntValue.of(t).vector();
-      fail();
-    } catch (IllegalArgumentException e) {
-      // ok
-    }
-  }
-  
-  @Test
-  public void getVectorInMatrix4d() {
-    int[][][][] matrix4d = new int[][][][]{
-      {
-        {
-          {1, 2}, {3, 4}, {5, 6}
-        },
-        {
-          {7, 8}, {9, 10}, {11, 12}
-        }
-      },
-      {
-        {
-          {13, 14}, {15, 16}, {17, 18}
-        },
-        {
-          {19, 20}, {21, 22}, {23, 24}
-        }
-      }
-    };
-    try (Tensor<Integer> t = Tensors.create(matrix4d)) {
-      IntValue vector = IntValue.of(t).at(1, 1, 1);
-      assertEquals(2, vector.size());
-      assertBufferEquals(IntBuffer.wrap(matrix4d[1][1][1]), vector.vector());
-    }
-  }
-
-  @Test
-  public void getMatrixInMatrix4d() {
-    int[][][][] matrix4d = new int[][][][]{
-      {
-        {
-          {1, 2}, {3, 4}
-        },
-        {
-          {5, 6}, {7, 8}
-        }
-      },
-      {
-        {
-          {9, 10}, {11, 12}
-        },
-        {
-          {13, 14}, {15, 16}
-        }
-      }
-    };
-    try (Tensor<Integer> t = Tensors.create(matrix4d)) {
-      IntValue matrix = IntValue.of(t).at(1, 1);
-      assertEquals(2L, matrix.size());
-      assertBufferEquals(IntBuffer.wrap(matrix4d[1][1][0]), matrix.at(0).vector());
-      assertBufferEquals(IntBuffer.wrap(matrix4d[1][1][1]), matrix.at(1).vector());
-    }
-  }
-
-  @Test
-  public void testMatrixWithOneWildcardInMatrix4d() {
-    int[][][][] matrix4d = new int[][][][]{
-      {
-        {
-          {1, 2}, {3, 4}, {5, 6}
-        },
-        {
-          {7, 8}, {9, 10}, {11, 12}
-        }
-      },
-      {
-        {
-          {13, 14}, {15, 16}, {17, 18}
-        },
-        {
-          {19, 20}, {21, 22}, {23, 24}
-        }
-      }
-    };
-    try (Tensor<Integer> t = Tensors.create(matrix4d)) {
-      IntValue matrix = IntValue.of(t).at(null, 1, 2);
-      assertEquals(2L, matrix.size());
-      assertBufferEquals(IntBuffer.wrap(matrix4d[0][1][2]), matrix.at(0).vector());
-      assertBufferEquals(IntBuffer.wrap(matrix4d[1][1][2]), matrix.at(1).vector());
-    }
-  }
-
-  @Test
-  public void testMatrixWithTwoWildcardsInMatrix4d() {
-    int[][][][] matrix4d = new int[][][][]{
-      {
-        {
-          {1, 2}, {3, 4}, {5, 6}
-        },
-        {
-          {7, 8}, {9, 10}, {11, 12}
-        }
-      },
-      {
-        {
-          {13, 14}, {15, 16}, {17, 18}
-        },
-        {
-          {19, 20}, {21, 22}, {23, 24}
-        }
-      }
-    };
-    try (Tensor<Integer> t = Tensors.create(matrix4d)) {
-      IntValue matrixA = IntValue.of(t).at(null, 1, null);
-      assertEquals(2L, matrixA.size());
-      IntValue matrixB = matrixA.at(1);
-      assertEquals(3L, matrixB.size());
-      IntValue matrixC = matrixB.at(2);
-      assertEquals(2L, matrixC.size());
-      assertBufferEquals(IntBuffer.wrap(matrix4d[1][1][2]), matrixC.vector());
-    }
-  }
-*/  
-  private static void assertBufferEquals(IntBuffer buf1, IntBuffer buf2) {
-    assertEquals(buf1.remaining(), buf2.remaining());
-    while (buf1.remaining() > 0) {
-      assertEquals(buf1.get(), buf2.get());
+      assertEquals(2, value.size());
+      assertEquals(2, value.at(1).size());
+      assertEquals(4, value.at(1, 1).size());
+      assertEquals(3, value.at(1, 1, 1).size());
+      assertEquals(2, value.at(1, 1, range(2, 4)).size());
+      assertEquals(41, value.at(1, 1, 1, 1).scalar());
+      assertEquals(IntBuffer.wrap(matrix4d[1][1][3]), value.at(1, 1, 3).vector());
+      assertEquals(35, value.at(1, FULL, range(2, 4)).at(0, 1, 1).scalar());
     }
   }
 }
